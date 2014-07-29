a5722a87c5c959fd3935b0c8608f7f40208c6e06
Request Tracker <rt@vm-rt.oeris.fr>
2014-07-29 22:13:45 +0200

    Changes for RT 3.8.1

--- lib/RT/Extension/HideWidgets.pm
+++ lib/RT/Extension/HideWidgets.pm
@@ -10,11 +10,11 @@ RT::Extension::HideWidgets - Allow admin to hide widgets per user, group and rol
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use RT::System;
 
